d441cf507c874005f3eed97727bed9283d7b1872
Jirayu Somcharoensin <pluem-jirayu@hotmail.com>
2021-02-28 06:21:49 +0700

    Entering nodejs

---
+++ hw_react/Frontend-Backend/todo-list-axios/src/component/pages/Index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import TodoList from '../TodoList/TodoList'
+
+
+export default function index() {
+    return (
+        <div>
+            <TodoList/>
+        </div>
+    )
+}

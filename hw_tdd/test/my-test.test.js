b12429d51611d4a1ccabaab0febd59130c61e40a
PluemJIR <pluem-jirayu@hotmail.com>
2021-04-25 02:13:13 +0700

    hw ttd

---
+++ hw_tdd/test/my-test.test.js
@@ -0,0 +1,9 @@
+const add = require('../my-test')
+
+test('two plus two is four', () => {
+    expect(add(2,2)).toBe(4);
+});
+
+test('two plus two is not three', () => {
+    expect(add(2,2)).not.toBe(3);
+});
